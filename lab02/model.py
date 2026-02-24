d1220d020c9cd6f7739f170156d6a7e3bf7e5e12
Squizly <petrkolesov010204@gmail.com>
2026-02-24 19:16:03 +0700

    upd: refactoring

--- lab02/model.py
+++ lab02/model.py
@@ -2,62 +2,67 @@ import numpy as np
 from numba import jit
 
 @jit(nopython=True)
-def simulate(rho, c, lam, Tl, Tr, T0, L, h, total_time, dt):
-    Nx = int(round(L / h)) 
+def simulate(rho, c, lam, Ta, Tn, T0, L, h, total_time, tau):
+    Nx = int(round(L / h))
     if Nx < 2: Nx = 2
-    dx = L / Nx
-    Nt = int(round(total_time / dt))
-    if Nt < 1: Nt = 1
+
+    h = L / Nx
+    
+    steps_n = int(round(total_time / tau))
+    if steps_n < 1: steps_n = 1
 
     T = np.full(Nx + 1, float(T0))
-    T[0] = float(Tl)
-    T[-1] = float(Tr)
+    T[0] = float(Ta)
+    T[Nx] = float(Tn)
 
-    # Коэффициенты для системы уравнений
-    A = lam / dx**2
-    C = A
-    B = 2 * lam / dx**2 + rho * c / dt
+    A_i = lam / h**2
+    C_i = A_i
+    B_i = (2 * lam / h**2) + (rho * c / tau)
 
     alpha = np.zeros(Nx + 1)
     beta = np.zeros(Nx + 1)
 
-    for _ in range(Nt):
+    for n in range(steps_n):
         alpha[0] = 0.0
-        beta[0] = float(Tl)
+        beta[0] = float(Ta)
 
         for i in range(1, Nx):
-            Fi = -(rho * c / dt) * T[i]
-            denom = B - C * alpha[i - 1]
-            alpha[i] = A / denom
-            beta[i] = (C * beta[i - 1] - Fi) / denom
+            F_i = -(rho * c / tau) * T[i]
 
-        T_new = np.empty(Nx + 1)
-        T_new[0] = float(Tl)
-        T_new[-1] = float(Tr)
+            denom = B_i - C_i * alpha[i - 1]
+
+            alpha[i] = A_i / denom
+            beta[i] = (C_i * beta[i - 1] - F_i) / denom
+
+        T_next = np.empty(Nx + 1)
+        T_next[Nx] = float(Tn)
+        T_next[0] = float(Ta)
 
         for i in range(Nx - 1, 0, -1):
-            T_new[i] = alpha[i] * T_new[i + 1] + beta[i]
-        T = T_new
+            T_next[i] = alpha[i] * T_next[i + 1] + beta[i]
+        
+        T = T_next
 
     return T, T[Nx // 2]
 
+
+# ТОЖЕ САМОЕ ДЛЯ GUI
 @jit(nopython=True)
-def calculate_next_step(T, alpha, beta, A, B, C, Nx, rho, c, dt, Tl, Tr):
-    # Один шаг для анимации
+def calculate_next_step(T, alpha, beta, A_i, B_i, C_i, Nx, rho, c, tau, Ta, Tn):
     alpha[0] = 0.0
-    beta[0] = float(Tl)
+    beta[0] = float(Ta)
 
     for i in range(1, Nx):
-        Fi = -(rho * c / dt) * T[i]
-        denom = B - C * alpha[i - 1]
-        alpha[i] = A / denom
-        beta[i] = (C * beta[i - 1] - Fi) / denom
+        F_i = -(rho * c / tau) * T[i]
+        denom = B_i - C_i * alpha[i - 1]
+        alpha[i] = A_i / denom
+        beta[i] = (C_i * beta[i - 1] - F_i) / denom
 
-    T_new = np.empty(Nx + 1)
-    T_new[0] = float(Tl)
-    T_new[-1] = float(Tr)
+    T_next = np.empty(Nx + 1)
+    T_next[Nx] = float(Tn)
+    T_next[0] = float(Ta)
 
     for i in range(Nx - 1, 0, -1):
-        T_new[i] = alpha[i] * T_new[i + 1] + beta[i]
+        T_next[i] = alpha[i] * T_next[i + 1] + beta[i]
 
-    return T_new
+    return T_next
